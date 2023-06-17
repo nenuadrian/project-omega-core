c559080c21437125d83bfa63472099757ecfad57
Adrian (Mircea) Nenu <adrian.nenu@gmail.com>
2023-06-17 12:35:11 +0100

    Update input.php

--- input.php
+++ input.php
@@ -16,6 +16,10 @@ abstract class Input {
         return $_POST[$field] ?? null;
     }
 
+    public static function cookie(string $field): ?string {
+        return isset($_COOKIE[$field]) ? $_COOKIE[$field] : null;
+      }
+
     public static function get(string $field): ?string {
         return isset($_GET[$field]) ? $_GET[$field] : null;
     }
