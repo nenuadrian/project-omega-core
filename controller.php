d65e30c3be46e25ca43716556ddb599daca9de54
Nenu Adrian <adrian.nenu@gmail.com>
2023-06-19 11:09:05 +0100

    format

--- controller.php
+++ controller.php
@@ -1,13 +1,17 @@
-<?php declare(strict_types=1);
+<?php declare (strict_types = 1);
 
-abstract class Controller {
+abstract class Controller
+{
     protected $tVars = [];
-    
-    protected function guard(): bool { return true; }
 
-    public function init(): void { }
-    
-    public function routes(?string $page, ?array $params = []): void {
+    protected function guard(): bool
+    {return true;}
+
+    public function init(): void
+    {}
+
+    public function routes( ? string $page,  ? array $params = []) : void
+    {
         $page = $page ? $page : 'index';
         $page = method_exists($this, $page) ? $page : 'index';
         if (method_exists($this, $page)) {
@@ -21,30 +25,36 @@ abstract class Controller {
                 $message = $error->getMessage();
                 $this->json(500, [], $message);
             }
-        } else die('404 - action not found');
+        } else {
+            die('404 - action not found');
+        }
+
     }
 
-    protected function redirect(string $url): void {
-        if(strpos($url, 'http') === false) {
-          $url = BASE_URL . '/' . $url;
+    protected function redirect(string $url) : void
+    {
+        if (strpos($url, 'http') === false) {
+            $url = BASE_URL . '/' . $url;
         }
-        
+
         if (!headers_sent()) {
             header('Location: ' . $url);
         }
     }
-  
-    protected function guardFail() {
-      $this->json(403);
+
+    protected function guardFail()
+    {
+        $this->json(403);
     }
 
-    protected function json(int $status, ?array $data = null, string $error = null) {
+    protected function json(int $status,  ? array $data = null, string $error = null)
+    {
         header("Access-Control-Allow-Origin: *");
 
         echo json_encode([
             "status" => $status,
             "data" => $data,
-            "error" => $error
+            "error" => $error,
         ]);
     }
 }
